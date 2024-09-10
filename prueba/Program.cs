c0eb1cde0d3097273d7664372368273435431b20
Jotacee20 <JOTACEE2589@GMAIL.COM>
2024-09-10 11:27:52 +0200

    Primer cambio en el Codigo

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Congratulations!");
+Console.Write("");
+Console.Write("you wrote your firts lines of code");
 Console.WriteLine("Hey, World!");
