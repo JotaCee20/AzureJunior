a5f446bcd9cf35fc8e63355ade46db47bbc09566
Jotacee20 <JOTACEE2589@GMAIL.COM>
2024-09-11 11:35:08 +0200

    Aprender Variables

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hey, World!");
+using System.Reflection.Metadata;
+
+Console.WriteLine("Congratulations!");
+Console.Write("");
+Console.WriteLine("you wrote your firts lines of code");
+
+var firstName = Bob;
+var messagesNumber = 3;
+var celsius = 34.4;
+
+firstName = "Bob";
+Console.WriteLine(firstName);
+
+
+firstName= "Liem";
+Console.WriteLine(firstName);
+
+firstName= "Isabella";
+Console.WriteLine(firstName);
+
+firstName= "Yasmin";
+Console.WriteLine(firstName);
+
+string message;
+
+message= "Hello World";
+Console.WriteLine(message);
+
+Console.Write("Hello," + firstName + "! You have" + messagesNumber + "messages in your inbox. The temperature is" + celsius + "celsius.");
